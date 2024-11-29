7233874cbe918d9c00e3ef3f061418678b65cfb7
Gabriel <gpereirasantos2002@gmail.com>
2024-11-28 21:23:40 -0500

    logger funcionando

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const routes = require('./routes'); // Importa el archivo de rutas
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swaggerConfig'); // Importa la configuración de Swagger
+const logger = require('./logger/logger');
+
 
 const corsOptions = {
   origin: '*', // Permitir todas las solicitudes de origen
@@ -16,30 +18,30 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json()); // Corrige el uso de express.json()
+app.use((req, res, next) => {
+  logger.info(`Solicitud entrante: ${req.method} ${req.url}`);
+  next();
+});
 app.use('/', routes); // Usa las rutas importadas
 
 // Configura Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000');
+  logger.info('Servidor iniciado en el puerto 3000');
 });
 
 // Instancia de Sequelize para conectarse a la base de datos
 const sequelize = require("./helpers/database.js");
 
-// Importaciones de los modelos 
-const Categorias = require("./helpers/models/categorias"); 
-const Ingredientes = require("./helpers/models/ingredientes"); 
-const Recetas = require("./helpers/models/recetas");
 
 // Sincronizar los modelos para verificar la conexión con la base de datos
 sequelize
   .sync({ alter: true })
   .then(() => {
-    console.log("Todos los modelos se sincronizaron correctamente.");
+    logger.info("Todos los modelos se sincronizaron correctamente.");
   })
   .catch((err) => {
-    console.log("Ha ocurrido un error al sincronizar los modelos: ", err);
+    logger.error("Ha ocurrido un error al sincronizar los modelos: ", err);
   });
-  //
+  
