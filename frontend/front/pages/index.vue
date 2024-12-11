8f440a08ebc464b828203278e65426dad2643b49
Gabriel <gpereirasantos2002@gmail.com>
2024-12-11 09:56:34 -0500

    header y pagina principal

---
+++ frontend/front/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col min-h-screen bg-gray-100">
+        <Header :name="name" :lastName="lastName" :fullName="fullName" />
+        <main class="flex-grow container mx-auto p-5 space-y-5">
+
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import {ref, computed } from "vue";
+
+const name = ref ("Lolo");
+const lastName = ref ("Joja");
+
+const fullName = computed(()=>  `$(name.value) $(lastName.value)`);
+</script>
