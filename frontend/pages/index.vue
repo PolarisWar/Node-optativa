2e8ada0290795af45f1380f4b17107398edcf216
Gabriel <gpereirasantos2002@gmail.com>
2024-12-12 14:19:24 -0500

    logo en pagina principal

---
+++ frontend/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative min-h-screen w-full">
+    <NuxtImg
+      class="absolute inset-0 w-4/5 h-4/5 object-contain mx-auto"
+      src="/logo.jpg" 
+      alt="Imagen de fondo"
+      format="webp"
+    />
+    <div class="fixed inset-0 bg-gray-800 bg-opacity-20 flex items-center justify-center"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style scoped>
+/* Puedes agregar estilos personalizados aquí si lo deseas */
+</style>
